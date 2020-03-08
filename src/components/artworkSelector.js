e88ac27c63c92a5490e667a3f708bb15c8d56ce4
Andy Jeong <unjin89@gmail.com>
2020-03-08 19:59:55 -0400

    created imagepreview and artworkselector components

---
+++ src/components/artworkSelector.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class artworkSelector extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <p>test</p>
+        )
+    }
+}
+
+export default artworkSelector;
