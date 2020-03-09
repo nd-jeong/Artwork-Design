c6b6be0c7343f2414fe17cf5478b5f3036188a80
Andy Jeong <unjin89@gmail.com>
2020-03-09 15:04:34 -0400

    completed lifting state of selected art id to app component's state

--- src/components/artworkSelector.js
+++ src/components/artworkSelector.js
@@ -4,6 +4,12 @@ import '../App.css';
 class ArtworkSelector extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        this.props.artSelect(e.target.id)
     }
 
     render() {
@@ -11,23 +17,13 @@ class ArtworkSelector extends React.Component {
         console.log(artworkArray)
         const artworkDisplay = artworkArray.map(artID => {
             return(
-            <div className='art-image' id={artID}></div>
+            <div className='art-image' id={artID} onClick={this.handleClick}></div>
             )
         })
         return(
             <div className='art-selector'>
                 <p>Art Selector</p>
                 {artworkDisplay}
-                {/* <div className='art-image' id='art-1'></div>
-                <div className='art-image' id='art-2'></div>
-                <div className='art-image' id='art-3'></div>
-                <div className='art-image' id='art-4'></div>
-                <div className='art-image' id='art-5'></div>
-                <div className='art-image' id='art-6'></div>
-                <div className='art-image' id='art-7'></div>
-                <div className='art-image' id='art-8'></div>
-                <div className='art-image' id='art-9'></div>
-                <div className='art-image' id='art-10'></div> */}
             </div>
         )
     }
